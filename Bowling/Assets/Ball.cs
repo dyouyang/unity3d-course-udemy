6a465d64a58210de764049ed0b5173f603b4e8f6
David Ouyang <David Ouyang>
2016-10-15 14:02:40 -0700

    Launch bowling ball on start.

---
+++ Bowling/Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float initialVelocity;
+
+    private AudioSource audioSource;
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        launchBall();
+    }
+
+    private void launchBall() {
+        audioSource.Play();
+        rigidbody.velocity = new Vector3(0, 0, initialVelocity);
+    }
+}
