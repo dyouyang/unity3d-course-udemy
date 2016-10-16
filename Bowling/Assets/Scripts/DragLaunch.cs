9e0a05ae1a1d5a301659f814e8497995a0e4eceb
David Ouyang <David Ouyang>
2016-10-15 18:38:46 -0700

    Add UI buttons to nudge ball right and left.

--- Bowling/Assets/Scripts/DragLaunch.cs
+++ Bowling/Assets/Scripts/DragLaunch.cs
@@ -7,10 +7,12 @@ public class DragLaunch : MonoBehaviour {
     private Ball ball;
     private float startTime;
     private Vector3 dragStart;
+    private bool launched;
 
     // Use this for initialization
     void Start () {
         ball = GetComponent<Ball> ();
+        launched = false;
 	}
 	
     public void OnDragStart() {
@@ -25,5 +27,14 @@ public class DragLaunch : MonoBehaviour {
         // Velocity is distance / time. Y value of drag difference is used for
         // ball's Z velocity down the lane.
         ball.launchBall(new Vector3(drag.x / timeDiff, 0, drag.y / timeDiff));
+        launched = true;
+    }
+
+    public void MoveStart(float xNudge) {
+        if (!launched) {
+            //ball.transform.position += new Vector3(xNudge, 0, 0);
+            ball.transform.Translate(new Vector3(xNudge, 0f, 0f));
+            
+        }
     }
 }
