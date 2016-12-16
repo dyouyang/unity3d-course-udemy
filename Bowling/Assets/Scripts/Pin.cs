a5a52c3c47d9639d5d85a3b2f672d060c15404cb
David Ouyang <dyouyang@dyouyang-macbookpro.roam.corp.google.com>
2016-12-15 17:27:35 -0800

    Add Pin function to get whether or not a pin is standing upright.
    We define a threshold such that if the pin's rotation is outside of 0 or 360
    by that amount, it is no longer considered standing.

---
+++ Bowling/Assets/Scripts/Pin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold = 10f;
+
+	public bool IsStanding() {
+		Vector3 eulerAngles = gameObject.transform.eulerAngles;
+		if (insideThreshold(eulerAngles.x) && insideThreshold(eulerAngles.z)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void Update() {
+		print (IsStanding ());
+	}
+
+	private bool insideThreshold(float angle) {
+		// Sometimes 360 is returned for upright instead of 0.
+		if (angle > -standingThreshold && angle < standingThreshold
+		    || angle > 360 - standingThreshold && angle < 360 + standingThreshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
