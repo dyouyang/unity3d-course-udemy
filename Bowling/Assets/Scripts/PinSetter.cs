3a08fc5eebc01192fd9c166bed8b9bc39b64d0ad
David Ouyang <dyouyang@dyouyang-macbookpro.roam.corp.google.com>
2016-12-15 18:06:13 -0800

    Add a PinSetter object that tracks the number of standing pins and updates the pin display text.

---
+++ Bowling/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+
+	public Text numPins;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		numPins.text = standingCount().ToString();
+	}
+
+	public int standingCount() {
+
+		int count = 0;
+		Pin[] pins = FindObjectsOfType<Pin> ();
+		foreach (Pin pin in pins) {
+			if (pin.IsStanding ()) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
