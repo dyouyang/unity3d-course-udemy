f5e98677ae98e948d92b63c8de06a6bb8a845d78
David Ouyang <dyouyang@dyouyang-macbookpro.roam.corp.google.com>
2016-12-28 21:38:11 -0800

    Add a simple test for action returned when bowling 10 pins.
    ActionMaster will be responsible for returning the desired
    game action depending on number of pins bowled.

---
+++ Bowling/Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ActionMasterTest {
+
+
+	[Test]
+	public void FailingTest() {
+		Assert.AreEqual (1, 2);
+	}
+
+	[Test]
+	public void BowlStrikeReturnsEndTurn() {
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual (ActionMaster.Action.EndTurn, actionMaster.Bowl (10));
+	}
+}
---
+++ Bowling/Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+	public enum Action {
+		Tidy,
+		Reset,
+		EndTurn,
+		EndGame
+	};
+
+	public Action Bowl (int pins) {
+
+		if (pins < 0 || pins > 10) {
+			throw new UnityException ("Bowled invalid number of pins (less than 0 or greater than 10");
+		}
+
+		if (pins == 10) {
+			return Action.EndTurn;
+		}
+
+		throw new UnityException ("Did not find a matching Action to return");
+	}
+}
