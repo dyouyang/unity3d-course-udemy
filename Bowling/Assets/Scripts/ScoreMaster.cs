c79176f744aff33208709bc5da58fea8bc06f652
David Ouyang <dyouyang@dyouyang-macbookpro.roam.corp.google.com>
2017-01-06 17:13:10 -0800

    Tweak ball shininess, add a new ScoreMaster (empty for now).

---
+++ Bowling/Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreMaster {
+
+	public static List<int> ScoreFrames(List<int> bowls) {
+		List<int> frameScores = new List<int> ();
+
+		return frameScores;
+	}
+}
