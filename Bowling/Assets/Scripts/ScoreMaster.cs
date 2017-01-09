ccfb4a00a4cdcdd3ce817abce5c693583d335040
David Ouyang <David Ouyang>
2017-01-08 22:51:32 -0800

    Early implementation of scoring
    Added failing ScoreMaster tests. Created a cumulative score
    function that returns a list of running totals. Incomplete
    implementation of frame scoring method.

--- Bowling/Assets/Scripts/ScoreMaster.cs
+++ Bowling/Assets/Scripts/ScoreMaster.cs
@@ -4,8 +4,25 @@ using System.Collections.Generic;
 
 public class ScoreMaster {
 
+    public static List<int> ScoreCumulative (List<int> bowls) {
+        List<int> cumulativeScores = new List<int>();
+        List<int> frameScores = ScoreFrames(bowls);
+        int totalSoFar = 0;
+        foreach (int frameScore in frameScores) {
+            totalSoFar += frameScore;
+            cumulativeScores.Add(totalSoFar);
+        }
+        return cumulativeScores;
+    }
+
 	public static List<int> ScoreFrames(List<int> bowls) {
 		List<int> frameScores = new List<int> ();
+        int currentFrameIndex = 0;
+        int currentBowlIndex = 0;
+
+        for (int i = currentBowlIndex; i < bowls.Count; i++) {
+
+        }
 
 		return frameScores;
 	}
