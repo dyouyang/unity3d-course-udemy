43a37a22f6d935f36136120ca43e6709bdabbb53
David Ouyang <dyouyang@dyouyang-macbookpro.roam.corp.google.com>
2017-01-30 18:17:00 -0800

    Format bowls array as a string of chars to display.
    The bowls array contains the pin bowls numbers, so we format
    them to standard bowling chars (/, X, -) in a string with each char
    corresponding to one bowl display box. Spaces are used when there is a strike,
    since the bowls array does not contain "empty" bowls due to strikes (except when
    the strike occurs in the last frame).

--- Bowling/Assets/Scripts/ScoreDisplay.cs
+++ Bowling/Assets/Scripts/ScoreDisplay.cs
@@ -19,8 +19,9 @@ public class ScoreDisplay : MonoBehaviour {
 	}
 
 	public void FillBowls(List<int> bowls) {
-		for (int bowlIndex = 0; bowlIndex < bowls.Count; bowlIndex++) {
-			bowlTexts [bowlIndex].text = bowls [bowlIndex].ToString ();
+		string formattedBowls = FormatRolls (bowls);
+		for (int bowlIndex = 0; bowlIndex < formattedBowls.Length; bowlIndex++) {
+			bowlTexts [bowlIndex].text = formattedBowls[bowlIndex].ToString();
 		}
 	}
 
@@ -28,5 +29,30 @@ public class ScoreDisplay : MonoBehaviour {
 		for (int frameIndex = 0; frameIndex < frames.Count; frameIndex++) {
 			scoreTexts [frameIndex].text = frames [frameIndex].ToString ();
 		}
+	}
+
+	public static string FormatRolls(List<int> bowls) {
+		string result = "";
+
+		for (int i = 0; i < bowls.Count; i++) {
+			// We use a current box index since bowls doesn't contain any empty bowls,
+			// for example on strikes.
+			int currentBox = result.Length + 1;
+			int currentBowl = bowls [i];
+
+			if (currentBowl == 0) {
+				result += "-";		// Empty
+			} else if (currentBox % 2 == 0 && bowls [i - 1] + currentBowl == 10) {
+				result += "/";		// Spare
+			} else if (currentBox >= 19 && currentBowl == 10) {
+				result += "X";		// Strike in last frame
+			} else if (currentBowl == 10) {
+				result += "X ";		// Strike anywhere else
+			} else {
+				result += currentBowl.ToString ();	// Normal bowl
+			}
+		}
+
+		return result;
 	}
 }
