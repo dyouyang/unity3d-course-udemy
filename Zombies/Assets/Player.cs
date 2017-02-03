4f41937ade48eb7510cf42e5d542e445ce921f18
David Ouyang <dyouyang@dyouyang-macbookpro.roam.corp.google.com>
2017-02-02 18:40:23 -0800

    Add a set of spawn points.
    Player script has a manual button for now
    to move the player's transform to a randomly chosen spawn point.

---
+++ Zombies/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Transform spawnPointsParent;
+
+	// Toggle "button" to respawn manually.
+	public bool respawn = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (respawn == true) {
+			Respawn ();
+		}
+	}
+
+	private void Respawn() {
+		Transform[] spawnPoints = spawnPointsParent.GetComponentsInChildren<Transform> ();
+
+		// Index 0 is the parent's transform, start at index 1.
+		Transform randomSpawn = spawnPoints [Random.Range (1, spawnPoints.Length)];
+		gameObject.transform.position = randomSpawn.transform.position;
+		respawn = false;
+	}
+}
