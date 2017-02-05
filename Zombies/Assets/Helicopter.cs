df22ea05fdd8b23483bf17cd893e3c93ac5ce860
David Ouyang <David Ouyang>
2017-02-04 19:08:22 -0800

    Add view zoom, helicopter, keybinds, and sounds
    View zoom is triggered on zoom hotkey and changes fov to 1.5x smaller.
    
    Add Helicopter game object, which maintains whether or not it
    has already been called.
    
    Helicopter can be called with H hotkey, which also plays a sound
    on the player's radio.
    
    The helicopter also has a rotor sound attached.

---
+++ Zombies/Assets/Helicopter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    public bool isCalled;
+
+	// Use this for initialization
+	void Start () {
+        isCalled = false;
+	}
+}
---
+++ Zombies/Assets/Radio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour {
+
+    public Helicopter helicopter;
+    public AudioClip callHeli;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("CallHeli") && !helicopter.isCalled) {
+            // Order here matters, is called must be set first otherwise future Updates
+            // may still retrieve false;
+            helicopter.isCalled = true;
+            audioSource.clip = callHeli;
+            audioSource.Play();
+        }
+    }
+}
