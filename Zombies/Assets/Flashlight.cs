58f4ad2ae8704afafa59b0f766d00b3d94667d6d
David Ouyang <douyang@gmail.com>
2017-02-13 00:41:16 -0800

    Add flashlight toggle, default input button F

---
+++ Zombies/Assets/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    Light flashlight;
+
+	// Use this for initialization
+	void Start () {
+        flashlight = GetComponent<Light>();
+        flashlight.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("ToggleFlashlight")) {
+            flashlight.enabled = !flashlight.enabled;
+        }
+	}
+}
