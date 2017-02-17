af68fb106284e4a52bd3547c06f44aee64f9a8e4
David Ouyang <douyang@gmail.com>
2017-02-16 22:10:19 -0800

    Add location hints to possible landing areas.
    Location hint can be brought up with Tab key, and is a 3D text that is billboarded to the player's camera.

---
+++ Zombies/Assets/Hint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint : MonoBehaviour {
+
+    MeshRenderer textRenderer;
+
+	// Use this for initialization
+	void Start () {
+        textRenderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Tab)) {
+            textRenderer.enabled = true;
+        } else {
+            textRenderer.enabled = false;
+        }
+	}
+}
