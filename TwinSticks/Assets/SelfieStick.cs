c53ff50afd628878b3b93aa4d9d4b54699f26eed
David Ouyang <douyang@gmail.com>
2017-02-18 17:16:39 -0800

    Add a camera that is controlled with the right controller stick.

---
+++ TwinSticks/Assets/SelfieStick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SelfieStick : MonoBehaviour {
+
+    public float rotateSpeed = 2f;
+
+    private GameObject player;
+    private Vector3 armRotationInEuler;
+
+    // Use this for initialization
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        armRotationInEuler = transform.rotation.eulerAngles;
+    }
+    // Update is called once per frame
+    void Update () {
+        armRotationInEuler.y += CrossPlatformInputManager.GetAxis("RHorizontal") * rotateSpeed;
+        armRotationInEuler.z += CrossPlatformInputManager.GetAxis("RVertical") * rotateSpeed;
+        transform.rotation = Quaternion.Euler(armRotationInEuler);
+        transform.position = player.transform.position;
+	}
+}
