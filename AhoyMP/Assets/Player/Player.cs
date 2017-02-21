e52648a92a5df5129948db839c994bcbaf6842db
David Ouyang <douyang@gmail.com>
2017-02-20 19:42:38 -0800

    Add input control to player object
    Note that this is incomplete. In a multiplayer scene, controls from one player will move both
    game objects.

---
+++ AhoyMP/Assets/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    Vector3 input = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        input.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        input.y = 0f;
+        input.z = CrossPlatformInputManager.GetAxis("Vertical");
+        transform.Translate(input);
+	}
+}
